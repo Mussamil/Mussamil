186d3bc3f068bec164ce5b16df7f8ee15638ac03
Mussamil <50843627+Mussamil@users.noreply.github.com>
2019-05-21 02:28:08 -0700

    Update A5.c

--- A5.c
+++ A5.c
@@ -8,9 +8,17 @@ if(ch == 'a'||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u'||ch =='A'||ch =='E'||ch
 {
 printf("\n %c is a vowel.",ch);
 }
-else
-{
-printf("\n %c is a consonant.",ch);
-}
-return 0;
+else if(ch == 'b'||ch == 'c' || ch == 'd' || ch == 'f' || ch == 'g' || ch == 'h' ||
+		ch == 'j' || ch == 'k' || ch == 'l' || ch == 'm' || ch == 'n'||ch == 'p' || ch == 'q' || ch == 'r' || ch == 's' || ch == 't' ||
+		ch == 'v' || ch == 'w' || ch == 'x' || ch == 'y' || ch == 'z'||ch == 'B'||ch == 'C' || ch == 'D' || ch == 'F' || ch == 'G' || ch == 'H' ||
+		ch == 'J' || ch == 'K' || ch == 'L' || ch == 'M' || ch == 'N'||ch == 'P' || ch == 'Q' || ch == 'R' || ch == 'S' || ch == 'T' ||
+		ch == 'V' || ch == 'W' || ch == 'X' || ch == 'Y' || ch == 'Z')
+	 {
+    	printf("\n %c is a consonant.", ch);
+	}
+	else
+	{
+	    printf("\n %c is a invalid.", ch);
+	}
+    return 0;
 }
