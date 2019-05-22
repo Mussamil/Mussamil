2046981420dac8a60ce0afe67336dd32df659dd3
Mussamil <50843627+Mussamil@users.noreply.github.com>
2019-05-22 12:58:01 -0700

    Create A54.c

---
+++ A54.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int b,i;
+	scanf("%d",&b);
+	if(b%2==0)
+	{
+	printf("yes");
+	}
+	else if(b==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+	printf("no");
+	}
+	return 0;
+}
