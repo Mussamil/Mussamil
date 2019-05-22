e5c2687814df8050f92fac8eb0d6ba3dd957e780
Mussamil <50843627+Mussamil@users.noreply.github.com>
2019-05-22 12:40:03 -0700

    Create A48.c

---
+++ A48.c
@@ -0,0 +1,14 @@
+    #include<stdio.h>
+void main()
+{
+    int m,i;
+    scanf("%d",&m);
+     if(m==1||m==2||m==3||m==4||m==5||m==6||m==7||m==8||m==9|| m==10)
+     {
+         printf("yes");
+     }
+      else
+      {
+          printf("no");
+ }
+}
